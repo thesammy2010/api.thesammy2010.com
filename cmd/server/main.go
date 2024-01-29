c321cac60eb530a69aa997a4d0ec77ea76f93f7d
TheSammy2010 <43960199+thesammy2010@users.noreply.github.com>
2024-01-29 00:54:36 +0000

    Allow config file to not exist (#12)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"github.com/alexlast/bunzap"
-	"github.com/felixge/httpsnoop"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/thesammy2010/api.thesammy2010.com/internal"
 	pb "github.com/thesammy2010/api.thesammy2010.com/proto/v1/squash"
@@ -15,7 +14,6 @@ import (
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/protobuf/encoding/protojson"
 	"net"
 	"net/http"
 )
@@ -23,18 +21,6 @@ import (
 var (
 	logger = zap.Must(zap.NewProduction())
 )
-
-// withLogger This wrapper snoops requests and prints out logs
-func withLogger(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		m := httpsnoop.CaptureMetrics(handler, writer, request)
-		logger.Info("Request",
-			zap.String("method", request.Method),
-			zap.Int("status", m.Code),
-			zap.String("path", request.URL.Path),
-		)
-	})
-}
 
 // InitModels Used to initialise db models if they don't exist
 func initModels(ctx context.Context, db bun.DB) {
@@ -94,25 +80,7 @@ func main() {
 		logger.Fatal("Failed to dial squashPlayerServer:", zap.Error(err))
 	}
 
-	gwmux := runtime.NewServeMux(
-		runtime.WithMarshalerOption("application/json+pretty", &runtime.JSONPb{
-			MarshalOptions: protojson.MarshalOptions{
-				Indent:    "  ",
-				Multiline: true,
-			},
-			UnmarshalOptions: protojson.UnmarshalOptions{
-				DiscardUnknown: true,
-			},
-		}),
-	)
-	prettier := func(h http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if _, ok := r.URL.Query()["pretty"]; ok {
-				r.Header.Set("Accept", "application/json+pretty")
-			}
-			h.ServeHTTP(w, r)
-		})
-	}
+	gwmux := runtime.NewServeMux(internal.GetMuxOpts(config)...)
 	// Register Squash Player proxy
 	err = pb.RegisterSquashPlayerServiceHandler(context.Background(), gwmux, conn)
 	if err != nil {
@@ -121,7 +89,7 @@ func main() {
 
 	gwServer := &http.Server{
 		Addr:    ":" + config.GatewayPort,
-		Handler: withLogger(prettier(gwmux)),
+		Handler: internal.HttpHandler(gwmux, config),
 	}
 	logger.Info("Serving gRPC-Gateway", zap.String("Port", config.GatewayPort))
 	logger.Fatal("Error serving gRPC Gateay", zap.Error(gwServer.ListenAndServe()))
--- internal/config.go
+++ internal/config.go
@@ -1,6 +1,14 @@
 package internal
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"strings"
+)
+
+var (
+	logger = zap.Must(zap.NewProduction())
+)
 
 // Config struct to hold config options
 type Config struct {
@@ -8,20 +16,35 @@ type Config struct {
 	GrpcPort    string `mapstructure:"GRPC_PORT"`
 	GatewayPort string `mapstructure:"PORT"`
 	DatabaseURL string `mapstructure:"DATABASE_URL"`
+	ApiKey      string `mapstructure:"API_KEY"`
+	// http handler flags
+	HandlerEnableLogging   bool `mapstructure:"HANDLERS_ENABLE_LOGGING"`
+	HandlerEnablePrettier  bool `mapstructure:"HANDLERS_PRETTIER"`
+	HandlerEnableBasicAuth bool `mapstructure:"HANDLERS_BASIC_AUTH"`
 }
 
 // LoadConfig function that loads config opts from files and env vars
 func LoadConfig() (config Config, err error) {
 	viper.SetDefault("PORT", "5000")
 	viper.SetDefault("GRPC_PORT", "8090")
+	viper.SetTypeByDefaultValue(true)
+	viper.SetEnvKeyReplacer(strings.NewReplacer(`.`, `_`))
 	viper.AddConfigPath(".")
 	viper.SetConfigName(".env")
 	viper.SetConfigType("env")
 	err = viper.ReadInConfig()
 	if err != nil {
-		return
+		logger.Warn("Failed to read config file", zap.String("configFile", viper.ConfigFileUsed()))
 	}
 	viper.AutomaticEnv()
 	err = viper.Unmarshal(&config)
+	logger.Info("Running with config",
+		zap.String("Environment", config.Environment),
+		zap.String("GrpcPort", config.GrpcPort),
+		zap.String("GatewayPort", config.GatewayPort),
+		zap.Bool("HandlerEnableLogging", config.HandlerEnableLogging),
+		zap.Bool("HandlerEnablePrettier", config.HandlerEnablePrettier),
+		zap.Bool("HandlerEnableBasicAuth", config.HandlerEnableBasicAuth),
+	)
 	return
 }
