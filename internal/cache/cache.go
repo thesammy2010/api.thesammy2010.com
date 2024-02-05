335fc8a9f443e3ee1aeef08be0376650711dd4e8
Leo Parkes-Neptune <43960199+thesammy2010@users.noreply.github.com>
2024-02-05 15:36:53 +0000

    Add trace logging + fix logging + add PATCH `/v1/squash/players`

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -24,29 +24,87 @@ func NewCache(defaultExpiration, cachePurgeTime int) *Cache {
 	}
 }
 
-func (c *Cache) GetSquashPlayer(id string) (*pb.SquashPlayer, bool) {
+func (c *Cache) GetSquashPlayer(id, trace string) (*pb.SquashPlayer, bool) {
 	data, ok := c.players.Get(id)
 	if ok {
-		logger.Debug("Resource found in cache", zap.String("Resource", "Player"), zap.String("ID", id))
+		logger.Debug("Resource found in cache",
+			zap.String("Resource", "Player"),
+			zap.String("ID", id),
+			zap.String("trace", trace),
+		)
 		var player pb.SquashPlayer
 		err := proto.Unmarshal(data.([]byte), &player)
 		if err != nil {
-			logger.Error("Failed to read cache for resource", zap.String("Resource", "Player"), zap.String("ID", id), zap.Error(err))
+			logger.Error("Failed to read cache for resource",
+				zap.String("Resource", "Player"),
+				zap.String("ID", id),
+				zap.String("trace", trace),
+				zap.Error(err),
+			)
 			return nil, false
 		}
 		return &player, true
 	}
-	logger.Debug("Player not in cache", zap.String("ID", id))
+	logger.Debug("Resource not in cache",
+		zap.String("Resource", "Player"),
+		zap.String("ID", id),
+		zap.String("trace", trace),
+	)
 	return nil, false
 }
 
-func (c *Cache) UpdateSquashPlayer(data *pb.SquashPlayer) bool {
+func (c *Cache) GetSquashPlayerList(offset string, trace string) (*pb.ListSquashPlayersResponse, bool) {
+	data, ok := c.players.Get("list-" + offset)
+	if ok {
+		logger.Debug("Resource found in cache",
+			zap.String("Resource", "PlayerList"),
+			zap.String("ID", offset),
+			zap.String("trace", trace),
+		)
+		var playerListResponse pb.ListSquashPlayersResponse
+		err := proto.Unmarshal(data.([]byte), &playerListResponse)
+		if err != nil {
+			logger.Error("Failed to read cache for resource",
+				zap.String("Resource", "PlayerList"),
+				zap.String("Offset", offset),
+				zap.String("trace", trace),
+				zap.Error(err),
+			)
+			return nil, false
+		}
+		return &playerListResponse, true
+	}
+	logger.Debug("Player not in cache",
+		zap.String("Resource", "PlayerList"),
+		zap.String("Offset", offset),
+		zap.String("trace", trace),
+	)
+	return nil, false
+}
+
+func (c *Cache) UpdateSquashPlayer(data *pb.SquashPlayer, trace string) bool {
 	serialised, err := proto.Marshal(data)
 	logger.Debug("Updating cache", zap.String("Resource", "Player"), zap.String("ID", data.Id))
 	if err != nil {
-		logger.Error("Error marshalling data to bytes for cache", zap.String("Resource", "Player"), zap.String("ID", data.Id))
+		logger.Error("Error marshalling data to bytes for cache",
+			zap.String("Resource", "Player"),
+			zap.String("ID", data.Id),
+			zap.String("trace", trace),
+			zap.Error(err),
+		)
 		return false
 	}
 	c.players.Set(data.Id, serialised, cache.DefaultExpiration)
+	return true
+}
+
+func (c *Cache) UpdateSquashPlayerList(offset string, data *pb.ListSquashPlayersResponse) bool {
+	serialised, err := proto.Marshal(data)
+	logger.Debug("Updating cache", zap.String("Resource", "PlayerList"), zap.String("Offset", offset))
+	if err != nil {
+		logger.Error("Error marshalling data to bytes for cache", zap.String("Resource", "PlayerList"), zap.String("Offset", offset))
+		return false
+	}
+	c.players.Set("list-"+offset, serialised, cache.DefaultExpiration)
 	return true
 }
